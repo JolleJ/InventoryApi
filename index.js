8b48edfaffde3bea778a9670cdfa39f8fb17e59d
JolleJ <Janssjoe@arcada.fi>
2019-01-11 19:47:51 +0200

    First functions

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
+const AWS = require('aws-sdk');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/status', function (req, res) {
+  res.sendStatus(200)
+})
+
+app.get('/availability', function (req, res) {
+  res.send("This api call is to get availability")
+})
+
+app.post('/increase', function (req, res) {
+  res.send("This api call is for increase availability")
+})
+
+app.post('/decrease', function (req, res) {
+  res.send("This api call is for increase decrease")
 })
 
 module.exports.handler = serverless(app);
