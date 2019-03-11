c1f1aedb4a913a1b854ac309bf5d4e765af2be79
JolleJ <Janssjoe@arcada.fi>
2019-03-11 19:53:40 +0200

    Fixed error

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ const passHeader = require("passport-headerapikey");
 
 
 
-passport.use(new HeaderAPIKeyStrategy(
-  { header: 'Authorization', prefix: 'Api-Key ' },
-  false,
-  function(apikey, done) {
-    User.findOne({ apikey: apikey }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
-
-
 app.get('/status', function(req, res){
   res.sendStatus(200);
 });
